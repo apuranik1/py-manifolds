Fix some naming bugs and derivative bugs

manifolds/euclidean.py
@@ -1,5 +1,6 @@
 from typing import Any, Sequence, TypeVar
 
+import jax
 import jax.numpy as jnp
 
 from .manifold import Chart, ChartPoint
@@ -10,7 +11,7 @@ P = TypeVar("P")
 
 
 class EuclideanPoint:
-    def __init__(self, coords: jnp.DeviceArray):
+    def __init__(self, coords: jax.Array):
         self.coords = coords
 
     @property
@@ -21,17 +22,17 @@ class EuclideanPoint:
 class IdChart(Chart[EuclideanPoint]):
     """Identity chart, mapping points to themselves"""
 
-    def point_to_coords(self, point: EuclideanPoint) -> jnp.DeviceArray:
+    def point_to_coords(self, point: EuclideanPoint) -> jax.Array:
         return point.coords
 
-    def coords_to_point(self, coords: jnp.DeviceArray) -> EuclideanPoint:
+    def coords_to_point(self, coords: jax.Array) -> EuclideanPoint:
         return EuclideanPoint(coords)
 
-    def to_array(self) -> jnp.DeviceArray:
+    def to_array(self) -> jax.Array:
         return jnp.array(0.)
 
     @classmethod
-    def of_array(cls: Any, arr: jnp.DeviceArray) -> "IdChart":
+    def of_array(cls: Any, arr: jax.Array) -> "IdChart":
         return IdChart()
 
 

manifolds/manifold.py
@@ -38,14 +38,14 @@ class Chart(Generic[P], metaclass=abc.ABCMeta):
     """
 
     @abc.abstractmethod
-    def coords_to_point(self, coords: jnp.DeviceArray) -> P:
+    def coords_to_point(self, coords: jax.Array) -> P:
         """Conversion from coordinates to a point in the domain.
 
         Raises OutOfDomain if the coords are not mapped to a valid point.
         """
 
     @abc.abstractmethod
-    def point_to_coords(self, point: P) -> jnp.DeviceArray:
+    def point_to_coords(self, point: P) -> jax.Array:
         """Convert a point to coordinates in this Chart.
 
         Raises OutOfDomain if the point is not in the domain of the chart.
@@ -53,12 +53,12 @@ class Chart(Generic[P], metaclass=abc.ABCMeta):
 
     @classmethod
     @abc.abstractmethod
-    def of_array(cls: Type[T_Chart], arr: jnp.DeviceArray) -> T_Chart:
+    def of_array(cls: Type[T_Chart], arr: jax.Array) -> T_Chart:
         """Deserialize an instance of this class from an array"""
         pass
 
     @abc.abstractmethod
-    def to_array(self) -> jnp.DeviceArray:
+    def to_array(self) -> jax.Array:
         """Serialize an instance of this class as an array"""
         pass
 
@@ -85,7 +85,7 @@ class Manifold(Generic[P], metaclass=abc.ABCMeta):
 class ChartPoint(Generic[P]):
     """A point in coordinates in a chart"""
 
-    coords: jnp.DeviceArray
+    coords: jax.Array
     chart: Chart[P]
 
     @staticmethod
@@ -134,7 +134,7 @@ class Tensor(Generic[P]):
     """
 
     point: ChartPoint[P]
-    t_coords: jnp.DeviceArray
+    t_coords: jax.Array
     n_contra: int
 
     @property
@@ -216,12 +216,12 @@ class Tensor(Generic[P]):
         indices along the inverse. It's more like a change of coordinates.
         """
 
-        def coord_map_forward(c: jnp.DeviceArray) -> jnp.DeviceArray:
+        def coord_map_forward(c: jax.Array) -> jax.Array:
             return chart.point_to_coords(
                 diffeo.forward(self.point.chart.coords_to_point(c))
             )
 
-        def coord_map_backward(c: jnp.DeviceArray) -> jnp.DeviceArray:
+        def coord_map_backward(c: jax.Array) -> jax.Array:
             return self.point.chart.point_to_coords(
                 diffeo.backward(chart.coords_to_point(c))
             )
@@ -253,15 +253,15 @@ class Tensor(Generic[P]):
         return self.diffeo_pushforward(ident, chart)
 
 
-class CovariantTensor(Tensor[P]):
-    def __init__(self, point: ChartPoint[P], t_coords: jnp.DeviceArray):
+class ContravariantTensor(Tensor[P]):
+    def __init__(self, point: ChartPoint[P], t_coords: jax.Array):
         super().__init__(point, t_coords, 0)
 
     def pushforward(
         self, morphism: Callable[[P], P_], chart: Chart[P_]
-    ) -> "CovariantTensor[P_]":
+    ) -> "ContravariantTensor[P_]":
         # see Tensor.diffeo_pushforward for explanation of details
-        def coord_map(c: jnp.DeviceArray) -> jnp.DeviceArray:
+        def coord_map(c: jax.Array) -> jax.Array:
             return chart.point_to_coords(morphism(self.point.chart.coords_to_point(c)))
 
         image = coord_map(self.point.coords)
@@ -271,20 +271,20 @@ class CovariantTensor(Tensor[P]):
             # we actually want left multiplication, so contract axis 1 of jacobian
             transformed_t = jnp.tensordot(transformed_t, jacobian, axes=([0], [1]))
 
-        return CovariantTensor(ChartPoint(image, chart), transformed_t)
+        return ContravariantTensor(ChartPoint(image, chart), transformed_t)
 
-    def covariant_prod(self, other: "CovariantTensor[P]") -> "CovariantTensor[P]":
+    def contravariant_prod(self, other: "ContravariantTensor[P]") -> "ContravariantTensor[P]":
         tensor = self.tensor_prod(other)
-        return CovariantTensor(tensor.point, tensor.t_coords)
+        return ContravariantTensor(tensor.point, tensor.t_coords)
 
 
-class ContravariantTensor(Tensor[P]):
-    def __init__(self, point: ChartPoint[P], t_coords: jnp.DeviceArray):
+class CovariantTensor(Tensor[P]):
+    def __init__(self, point: ChartPoint[P], t_coords: jax.Array):
         super().__init__(point, t_coords, len(t_coords.shape))
 
     def pullback(
         self, morphism: Callable[[P_], P], preimage: ChartPoint[P_]
-    ) -> "ContravariantTensor[P_]":
+    ) -> "CovariantTensor[P_]":
         """Compute a pullback of a covariant tensor along [morphism].
 
         Requires a preimage of this point to be provided. Correctness of the preimage
@@ -292,7 +292,7 @@ class ContravariantTensor(Tensor[P]):
         """
         # see Tensor.diffeo_pushforward for explanation of details
 
-        def coord_map(c: jnp.DeviceArray) -> jnp.DeviceArray:
+        def coord_map(c: jax.Array) -> jax.Array:
             return self.point.chart.point_to_coords(
                 morphism(preimage.chart.coords_to_point(c))
             )
@@ -302,13 +302,13 @@ class ContravariantTensor(Tensor[P]):
         for _ in range(self.n_contra):
             transformed_t = jnp.tensordot(transformed_t, jacobian, axes=([0], [0]))
 
-        return ContravariantTensor(preimage, transformed_t)
+        return CovariantTensor(preimage, transformed_t)
 
-    def contravariant_prod(
-        self, other: "ContravariantTensor[P]"
-    ) -> "ContravariantTensor[P]":
+    def covariant_prod(
+        self, other: "CovariantTensor[P]"
+    ) -> "CovariantTensor[P]":
         tensor = self.tensor_prod(other)
-        return ContravariantTensor(tensor.point, tensor.t_coords)
+        return CovariantTensor(tensor.point, tensor.t_coords)
 
 
 @dataclass(frozen=True)
@@ -316,14 +316,14 @@ class Tangent(Generic[P]):
     """An element of the tangent bundle on a manifold in some chart"""
 
     point: ChartPoint[P]
-    v_coords: jnp.DeviceArray
+    v_coords: jax.Array
 
     def pushforward(
         self, morphism: Callable[[P], P_], chart: Chart[P_]
     ) -> "Tangent[P_]":
         """Compute pushforward along a morphism, expressed in the given chart"""
 
-        def coord_map(c: jnp.DeviceArray) -> jnp.DeviceArray:
+        def coord_map(c: jax.Array) -> jax.Array:
             return chart.point_to_coords(morphism(self.point.chart.coords_to_point(c)))
 
         image, tangent = jax.jvp(coord_map, [self.point.coords], [self.v_coords])
@@ -342,8 +342,8 @@ class Tangent(Generic[P]):
     # The user could define this, but it's identical to a dot product
 
     def derive_autodiff(
-        self, scalar_field: Callable[[P], jnp.DeviceArray]
-    ) -> Tuple[jnp.DeviceArray, jnp.DeviceArray]:
+        self, scalar_field: Callable[[P], jax.Array]
+    ) -> Tuple[jax.Array, jax.Array]:
         """Take the derivative of a scalar field with respect to this tangent.
 
         If scalar_field returns an array instead of a scalar, takes the derivative
@@ -356,12 +356,11 @@ class Tangent(Generic[P]):
           image.
         """
 
-        def coord_map(c: jnp.DeviceArray) -> float:
+        def coord_map(c: jax.Array) -> float:
             return scalar_field(self.point.chart.coords_to_point(c))
 
         # I think this is equivalent to pushforward to Euclidean space
-        _, derivative = jax.jvp(coord_map, [self.point.coords], [self.v_coords])
-        return derivative
+        return jax.jvp(coord_map, [self.point.coords], [self.v_coords])
 
     @staticmethod
     def of_tensor_exn(t: Tensor[P]) -> "Tangent[P]":
@@ -374,14 +373,14 @@ class Cotangent(Generic[P]):
     """An element of the cotangent bundle on a manifold in some chart"""
 
     point: ChartPoint[P]
-    v_coords: jnp.DeviceArray
+    v_coords: jax.Array
 
     def pullback(
         self, morphism: Callable[[P_], P], preimage: ChartPoint[P_]
     ) -> "Cotangent[P_]":
         """Compute pullback along a morphism in the given chart"""
 
-        def coord_map(c: jnp.DeviceArray):
+        def coord_map(c: jax.Array):
             return self.point.chart.point_to_coords(
                 morphism(preimage.chart.coords_to_point(c))
             )
@@ -396,7 +395,7 @@ class Cotangent(Generic[P]):
         )
         return self.pullback(lambda p: p, image)
 
-    def dot(self, vec: Tangent[P]) -> jnp.DeviceArray:
+    def dot(self, vec: Tangent[P]) -> jax.Array:
         """Return inner product as a 0-D array"""
         return jnp.dot(self.v_coords, vec.v_coords)
 

manifolds/riemannian.py
@@ -22,7 +22,7 @@ P = TypeVar("P")
 P_ = TypeVar("P_")
 
 
-class PseudoMetric(ContravariantTensor[P]):
+class PseudoMetric(CovariantTensor[P]):
     """A psuedo-Riemannian metric on a manifold. For correct semantics, coords must be
     symmetric and non-singular, but this is not verified. If coords is
     positive-definite, then this will be an true Riemannian metric.
@@ -30,7 +30,7 @@ class PseudoMetric(ContravariantTensor[P]):
     Convenience functions are provided for operations on tangent and cotangent vectors.
     """
 
-    def __init__(self, point: ChartPoint[P], coords: jnp.DeviceArray):
+    def __init__(self, point: ChartPoint[P], coords: jax.Array):
         """Create a metric from coordinates. """
         if not len(coords.shape) == 2:
             raise ValueError("Metric must have exactly two indices")
@@ -58,12 +58,12 @@ class PseudoMetric(ContravariantTensor[P]):
         coords = jnp.linalg.solve(self.t_coords, vector.v_coords)
         return Tangent(self.point, coords)
 
-    def inverse(self) -> CovariantTensor[P]:
-        """Returns a covariant 2-tensor"""
+    def inverse(self) -> ContravariantTensor[P]:
+        """Returns a contravariant 2-tensor"""
         coords = jnp.linalg.inv(self.t_coords)
-        return CovariantTensor(self.point, coords)
+        return ContravariantTensor(self.point, coords)
 
-    def inner(self, a: Tangent[P], b: Tangent[P]) -> jnp.DeviceArray:
+    def inner(self, a: Tangent[P], b: Tangent[P]) -> jax.Array:
         """Compute inner product of vectors, as 0-D array"""
         return jnp.dot(a.v_coords, jnp.dot(self.t_coords, b.v_coords))
 
@@ -107,7 +107,7 @@ def levi_civita(
 ) -> Christoffel[P]:
     # Compute the Levi-Civita connection for a pseudo-Riemannian manifold
     # formula: Gamma^k_ij = 0.5 g^kl (-d_l g_ij + d_i g_jl + d_j g_il)
-    def metric_matrix(coords: jnp.DeviceArray) -> jnp.DeviceArray:
+    def metric_matrix(coords: jax.Array) -> jax.Array:
         return manifold.metric_in_chart(ChartPoint(coords, point.chart)).t_coords
 
     jacobian = jax.jacfwd(metric_matrix)(point.coords)

manifolds/sphere.py
@@ -1,5 +1,6 @@
 from typing import Sequence
 
+import jax
 import jax.numpy as jnp
 
 from manifolds.euclidean import EuclideanPoint, EuclideanSpace
@@ -14,7 +15,7 @@ from manifolds.riemannian import (
 class SpherePoint:
     """A point on an n-sphere, represented as an embedding into R^{n+1}"""
 
-    def __init__(self, coords: jnp.DeviceArray):
+    def __init__(self, coords: jax.Array):
         self.coords = coords
 
 
@@ -24,10 +25,10 @@ class StereographicChart(Chart[SpherePoint]):
     Projects the sphere to the plane from point (0, ..., 0, signed_radius)
     """
 
-    def __init__(self, signed_radius: jnp.DeviceArray) -> None:
+    def __init__(self, signed_radius: jax.Array) -> None:
         self.signed_radius = signed_radius
 
-    def point_to_coords(self, point: SpherePoint) -> jnp.DeviceArray:
+    def point_to_coords(self, point: SpherePoint) -> jax.Array:
         (dim,) = point.coords.shape
         if dim < 2:
             raise ValueError("Sphere of dimension < 1 is not supported")
@@ -43,7 +44,7 @@ class StereographicChart(Chart[SpherePoint]):
         p = u / lam
         return p
 
-    def coords_to_point(self, coords: jnp.DeviceArray) -> SpherePoint:
+    def coords_to_point(self, coords: jax.Array) -> SpherePoint:
         (dim,) = coords.shape
         if dim < 1:
             raise ValueError("Sphere of dimension < 1 is not supported")
@@ -66,11 +67,11 @@ class StereographicChart(Chart[SpherePoint]):
         embed_coords = jnp.append(u, jnp.array([v]))
         return SpherePoint(embed_coords)
 
-    def to_array(self) -> jnp.DeviceArray:
+    def to_array(self) -> jax.Array:
         return self.signed_radius
 
     @classmethod
-    def of_array(cls, arr: jnp.DeviceArray) -> "StereographicChart":
+    def of_array(cls, arr: jax.Array) -> "StereographicChart":
         assert arr.shape == ()
         return StereographicChart(arr)
 